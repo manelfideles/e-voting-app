e8dfc702bb5a3b77c5a682d82e5b0490d45c6758
Manuel Fideles <manuelfideles77@gmail.com>
2021-04-02 18:53:23 +0100

    Merge w/ zemario

---
+++ Session.java
@@ -0,0 +1,13 @@
+public class Session {
+    private String TERMINAL_ADDR;
+    private String VOTE_ADDR;
+    private int PORT;
+    private String CC;
+    private String PW;
+
+    public Session() {
+
+    }
+
+    //
+}
